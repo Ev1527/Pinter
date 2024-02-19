07670e69192df7f94b45340fe9cca575fb802820
VictorBorisov <victorboris195@gmail.com>
2024-02-19 09:17:02 -0300

    search map

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,28 +1,28 @@
-import React, { useEffect } from "react";
-import "./App.css";
-import axios from "axios";
-import { Route, Routes } from "react-router-dom";
-import Navigation from "../components/navigation/Navigation";
-import Error from "../components/404/Error";
-import Registration from "../components/auth/Registration";
-import Authorization from "../components/auth/Authorization";
-import { useAppDispatch } from "../redux/store";
-import {  checkAuth } from "../components/auth/authSlice";
-import MainPage from "../components/main/MainPage";
-import PartiesList from "../components/party/PartiesList";
-import Map from "../components/map/Map";
-import UserProfile from "../components/profile/UserProfile";
-import PartyPage from "../components/party/PartyPage";
-import RoomPage from "../components/room/RoomPage";
-import ThreeSteps from "../components/steps/ThreeSteps";
-import { loadParties } from "../components/party/partySlice";
-import ChatPage from "../components/chat/ChatPage";
-import { allAccessTables } from "../components/room/roomSlice";
+import React, { useEffect } from 'react';
+import './App.css';
+import axios from 'axios';
+import { Route, Routes } from 'react-router-dom';
+import Navigation from '../components/navigation/Navigation';
+import Error from '../components/404/Error';
+import Registration from '../components/auth/Registration';
+import Authorization from '../components/auth/Authorization';
+import { useAppDispatch } from '../redux/store';
+import { checkAuth } from '../components/auth/authSlice';
+import MainPage from '../components/main/MainPage';
+import PartiesList from '../components/party/PartiesList';
+import Map from '../components/map/Map';
+import UserProfile from '../components/profile/UserProfile';
+import PartyPage from '../components/party/PartyPage';
+import RoomPage from '../components/room/RoomPage';
+import ThreeSteps from '../components/steps/ThreeSteps';
+import { loadParties } from '../components/party/partySlice';
+import ChatPage from '../components/chat/ChatPage';
+import { allAccessTables } from '../components/room/roomSlice';
 
 function App(): JSX.Element {
   const dispatch = useAppDispatch();
-  // axios.defaults.baseURL = "http://localhost:3001";
-  axios.defaults.baseURL = "https://pinter.fun";
+  axios.defaults.baseURL = 'http://localhost:3001';
+  // axios.defaults.baseURL = "https://pinter.fun";
   axios.defaults.withCredentials = true;
 
   useEffect(() => {
@@ -33,19 +33,19 @@ function App(): JSX.Element {
 
   return (
     <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route path="/auth/registration" element={<Registration />} />
-        <Route path="/auth/authorization" element={<Authorization />} />
+      <Route path='/' element={<Navigation />}>
+        <Route path='/auth/registration' element={<Registration />} />
+        <Route path='/auth/authorization' element={<Authorization />} />
         <Route index element={<MainPage />} />
-        <Route path="/parties" element={<PartiesList />} />
-        <Route path="/steps" element={<ThreeSteps />} />
-        <Route path="/contacts" element={<Map />} />
+        <Route path='/parties' element={<PartiesList />} />
+        <Route path='/steps' element={<ThreeSteps />} />
+        <Route path='/contacts' element={<Map />} />
       </Route>
-      <Route path="/profile" element={<UserProfile />} />
-      <Route path="/parties/:partyId" element={<PartyPage />} />
-      <Route path="/room" element={<RoomPage />} />
-      <Route path="/chat" element={<ChatPage/>}/>
-      <Route path="*" element={<Error />} />
+      <Route path='/profile' element={<UserProfile />} />
+      <Route path='/parties/:partyId' element={<PartyPage />} />
+      <Route path='/room' element={<RoomPage />} />
+      <Route path='/chat' element={<ChatPage />} />
+      <Route path='*' element={<Error />} />
     </Routes>
   );
 }
--- client/src/components/map/Map.tsx
+++ client/src/components/map/Map.tsx
@@ -1,44 +1,228 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './styles/Map.module.scss';
 // import map from './styles/map_img.png';
+import { debounce } from 'lodash';
 import Footer from '../footer/Footer';
 import MapComponent from './MapComponent';
 
+const center = [55.75393, 37.621558];
+
+const places = [
+  {
+    id: 1,
+    coordinate: [55.743611, 37.653889],
+    name: 'Московский театр на Таганке',
+  },
+  { id: 2, coordinate: [55.764105, 37.656164], name: 'Довлатов' },
+  { id: 3, coordinate: [55.758471, 37.659687], name: 'Torro Grill' },
+  { id: 4, coordinate: [55.778244, 37.587355], name: 'Torro Grill' },
+  { id: 5, coordinate: [55.760532, 37.619825], name: 'Большой театр' },
+  { id: 6, coordinate: [55.813798, 37.633858], name: 'Суши мастер' },
+  { id: 7, coordinate: [55.741643, 37.652854], name: 'Стимпанк бар' },
+];
+
+const images = [...Array(26)].map((_, i) => {
+  const letter = String.fromCharCode(i + 97);
+  return `https://img.icons8.com/ios-filled/2x/marker-${letter}.png`;
+});
+
+// export default function Map(): JSX.Element {
+//   const [searchValue, setSearchValue] = useState('');
+//   const [mapCenter, setMapCenter] = useState(center);
+
+//   const handleSearchChange = (value: string) => {
+//     setSearchValue(value);
+
+//     // Реализуйте логику поиска и установки нового центра карты на основе значения поиска
+//     // Например, можно фильтровать places и устанавливать центр на первый результат
+//     // Это простой пример, вы можете настроить логику поиска под свои требования
+//     const searchResults = places.filter((place) =>
+//       place.name.toLowerCase().includes(value.toLowerCase())
+//     );
+
+//     if (searchResults.length > 0) {
+//       setMapCenter(searchResults[0].coordinate);
+//     }
+//   };
+
+//   return (
+//     <>
+//       <div className={styles.map__container}>
+//         <h1>Найти место</h1>
+
+//         <div className={styles.content}>
+//           <div className={styles.search}>
+//             <input
+//               type='text'
+//               placeholder='Введите адрес'
+//               value={searchValue}
+//               onChange={(e) => handleSearchChange(e.target.value)}
+//             />
+//             <p>Результаты поиска</p>
+//             <div className={styles.search__results}>
+//               <div>
+//                 <p>Адрес бара номер один</p>
+//                 <p className={styles.on_map}>Показать на карте</p>
+//               </div>
+//               <div>
+//                 <p>Адрес бара номер два</p>
+//                 <p className={styles.on_map}>Показать на карте</p>
+//               </div>
+//               <div>
+//                 <p>Адрес бара номер три</p>
+//                 <p className={styles.on_map}>Показать на карте</p>
+//               </div>
+//             </div>
+//           </div>
+
+//           <div className={styles.map}>
+//             <MapComponent center={mapCenter} places={places} />
+//             <div className={styles.map__overlay}></div>
+//           </div>
+//         </div>
+//       </div>
+//       <Footer />
+//     </>
+//   );
+// }
+
+//
+
+interface Place {
+  id: number;
+  coordinate: number[];
+  name: string;
+}
+
+// export default function Map(): JSX.Element {
+//   const [searchValue, setSearchValue] = useState('');
+//   const [mapCenter, setMapCenter] = useState(center);
+//   const [searchResults, setSearchResults] = useState<Place[]>([]);
+
+//   let debounceTimeout: NodeJS.Timeout | null = null;
+
+//   const handleSearchChange = debounce((value: string) => {
+//     setSearchValue(value);
+
+//     if (debounceTimeout) {
+//       clearTimeout(debounceTimeout);
+//     }
+
+//     debounceTimeout = setTimeout(() => {
+//       // Реализуйте логику поиска и установки нового центра карты на основе значения поиска
+//       const filteredResults = places.filter((place) =>
+//         place.name.toLowerCase().includes(value.toLowerCase())
+//       );
+
+//       setSearchResults(filteredResults);
+
+//       if (filteredResults.length > 0) {
+//         setMapCenter(filteredResults[0].coordinate);
+//       } else {
+//         // Если результаты пусты, вернуть карту в центр
+//         setMapCenter(center);
+//       }
+//     }, 300);
+//   }, 300);
+
+//   return (
+//     <>
+//       <div className={styles.map__container}>
+//         <h1>Найти место</h1>
+
+//         <div className={styles.content}>
+//           <div className={styles.search}>
+//             <input
+//               type='text'
+//               placeholder='Введите адрес'
+//               value={searchValue}
+//               onChange={(e) => handleSearchChange(e.target.value)}
+//             />
+//             <p>Результаты поиска</p>
+//             <div className={styles.search__results}>
+//               {searchResults.slice(0, 5).map((result) => (
+//                 <div key={result.id}>
+//                   <p>{result.name}</p>
+//                   <p className={styles.on_map}>Показать на карте</p>
+//                 </div>
+//               ))}
+//             </div>
+//           </div>
+
+//           <div className={styles.map}>
+//             <MapComponent center={mapCenter} places={places} images={images} />
+//             <div className={styles.map__overlay}></div>
+//           </div>
+//         </div>
+//       </div>
+//       <Footer />
+//     </>
+//   );
+// }
+
 export default function Map(): JSX.Element {
+  const [searchValue, setSearchValue] = useState('');
+  const [mapCenter, setMapCenter] = useState(center);
+  const [searchResults, setSearchResults] = useState<Place[]>([]);
 
-    return (
-        <>
-            <div className={styles.map__container}>
-                <h1>Найти место</h1>
-                
-                <div className={styles.content}>
-                    <div className={styles.search}>
-                        <input type="text" placeholder='Введите адрес' />
-                        <p>Результаты поиска</p>
-                        <div className={styles.search__results}>
-                            <div>
-                                <p>Адрес бара номер один</p>
-                                <p className={styles.on_map}>Показать на карте</p>
-                            </div>
-                            <div>
-                                <p>Адрес бара номер два</p>
-                                <p className={styles.on_map}>Показать на карте</p>
-                            </div>
-                            <div>
-                                <p>Адрес бара номер три</p>
-                                <p className={styles.on_map}>Показать на карте</p>
-                            </div>
-                        </div>
-                    </div>
+  let debounceTimeout: NodeJS.Timeout | null = null;
 
-                    <div className={styles.map}>
-                        {/* <img src={map} alt="map img" /> */}
-                        <MapComponent />
-                        <div className={styles.map__overlay}></div>
-                    </div>
+  const handleSearchChange = (value: string) => {
+    setSearchValue(value);
+
+    if (debounceTimeout) {
+      clearTimeout(debounceTimeout);
+    }
+
+    debounceTimeout = setTimeout(() => {
+      const filteredResults = places.filter((place) =>
+        place.name.toLowerCase().includes(value.toLowerCase())
+      );
+
+      if (filteredResults.length > 0) {
+        setMapCenter(filteredResults[0].coordinate);
+        setSearchResults(filteredResults);
+      } else {
+        setMapCenter(center);
+        setSearchResults([]);
+      }
+    }, 100);
+  };
+
+  return (
+    <>
+      <div className={styles.map__container}>
+        <h1>Найти место</h1>
+
+        <div className={styles.content}>
+          <div className={styles.search}>
+            <input
+              type='text'
+              placeholder='Введите адрес'
+              value={searchValue}
+              onChange={(e) => handleSearchChange(e.target.value)}
+            />
+            <p>Результаты поиска</p>
+            <div className={styles.search__results}>
+              {searchResults.slice(0, 5).map((result) => (
+                <div key={result.id}>
+                  <p>{result.name}</p>
+                  <p className={styles.on_map}>Показать на карте</p>
                 </div>
+              ))}
             </div>
-            <Footer />
-        </>
-    )
+          </div>
+          <div className={styles.map}>
+            <MapComponent
+              center={mapCenter}
+              places={searchResults}
+              images={images}
+            />
+            <div className={styles.map__overlay}></div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
